f1e7ebbbc8f613e991aa521682d1dfc565b86b91
Marshall Conover <marzhall.o+github@gmail.com>
2019-10-27 13:33:39 -0400

    Basic working queue - echo to file to queue, and read to have it show the queued message. All messages pop out at once, which needs to be fixed.

---
+++ file.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"time"
+)
+
+var internal_queue = make(chan []byte, 2)
+
+// Turn Go types into files
+
+type fakefile struct {
+	v      interface{}
+	offset int64
+	set    func(s string)
+}
+
+func (f *fakefile) ReadAt(p []byte, off int64) (int, error) {
+	select {
+	case temp := <-internal_queue:
+		n := copy(p, temp)
+		return n, nil
+	default:
+		return 0, nil
+	}
+}
+
+func (f *fakefile) Write(p []byte) (int, error) {
+	/*buf, ok := f.v.(*bytes.Buffer)
+	if !ok {
+		return 0, errors.New("not supported")
+	}
+	if off != f.offset {
+		return 0, errors.New("no seeking")
+	}
+	n, err := buf.Write(p)
+	f.offset += int64(n)
+	return n, err
+	*/
+	go func() {
+		internal_queue <- p
+	}()
+
+	return len(p), nil
+}
+
+func (f *fakefile) WriteAt(p []byte, off int64) (int, error) {
+	/*buf, ok := f.v.(*bytes.Buffer)
+	if !ok {
+		return 0, errors.New("not supported")
+	}
+	if off != f.offset {
+		return 0, errors.New("no seeking")
+	}
+	n, err := buf.Write(p)
+	f.offset += int64(n)
+	return n, err
+	*/
+	go func() {
+		internal_queue <- p
+	}()
+
+	return len(p), nil
+}
+
+func (f *fakefile) Close() error {
+	if f.set != nil {
+		f.set(fmt.Sprint(f.v))
+	}
+	return nil
+}
+
+func (f *fakefile) size() int64 {
+	return 0
+}
+
+type stat struct {
+	name string
+	file *fakefile
+}
+
+func (s *stat) Name() string     { return s.name }
+func (s *stat) Sys() interface{} { return s.file }
+
+func (s *stat) ModTime() time.Time {
+	return time.Now().Truncate(time.Hour)
+}
+
+func (s *stat) IsDir() bool {
+	return s.Mode().IsDir()
+}
+
+func (s *stat) Mode() os.FileMode {
+	switch s.file.v.(type) {
+	case map[string]interface{}:
+		return os.ModeDir | 0755
+	case []interface{}:
+		return os.ModeDir | 0755
+	}
+	return 0644
+}
+
+func (s *stat) Size() int64 {
+	return s.file.size()
+}
+
+type dir struct {
+	c    chan stat
+	done chan struct{}
+}
+
+func mkdir(val interface{}) *dir {
+	c := make(chan stat, 10)
+	done := make(chan struct{})
+	go func() {
+		if m, ok := val.(map[string]interface{}); ok {
+		LoopMap:
+			for name, v := range m {
+				select {
+				case c <- stat{name: name, file: &fakefile{v: v}}:
+				case <-done:
+					break LoopMap
+				}
+			}
+		} else if a, ok := val.([]interface{}); ok {
+		LoopArray:
+			for i, v := range a {
+				name := strconv.Itoa(i)
+				select {
+				case c <- stat{name: name, file: &fakefile{v: v}}:
+				case <-done:
+					break LoopArray
+				}
+			}
+		}
+		close(c)
+	}()
+	return &dir{
+		c:    c,
+		done: done,
+	}
+}
+
+func (d *dir) Readdir(n int) ([]os.FileInfo, error) {
+	var err error
+	fi := make([]os.FileInfo, 0, 10)
+	for i := 0; i < n; i++ {
+		s, ok := <-d.c
+		if !ok {
+			err = io.EOF
+			break
+		}
+		fi = append(fi, &s)
+	}
+	return fi, err
+}
+
+func (d *dir) Close() error {
+	close(d.done)
+	return nil
+}
---
+++ kyuu.go
@@ -0,0 +1,190 @@
+// Command jsonfs allows for the consumption and manipulation of a JSON
+// object as a file system hierarchy.
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"strconv"
+	"strings"
+
+	"aqwari.net/net/styx"
+)
+
+var (
+	addr    = flag.String("a", ":5640", "Port to listen on")
+	debug   = flag.Bool("D", false, "trace 9P messages")
+	verbose = flag.Bool("v", false, "print extra info")
+)
+
+type server struct {
+	file map[string]interface{}
+}
+
+var logrequests styx.HandlerFunc = func(s *styx.Session) {
+	for s.Next() {
+		log.Printf("%q %T %s", s.User, s.Request(), s.Request().Path())
+	}
+}
+
+func main() {
+	flag.Parse()
+	log.SetPrefix("")
+	log.SetFlags(0)
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	var srv server
+	fsLayout := fmt.Sprintf(`{"%s": "s"}"`, flag.Arg(0))
+
+	layoutreader := strings.NewReader(fsLayout)
+	d := json.NewDecoder(layoutreader)
+	if err := d.Decode(&srv.file); err != nil {
+		log.Fatal(err)
+	}
+	var styxServer styx.Server
+	if *verbose {
+		styxServer.ErrorLog = log.New(os.Stderr, "", 0)
+	}
+	if *debug {
+		styxServer.TraceLog = log.New(os.Stderr, "", 0)
+	}
+	styxServer.Addr = *addr
+	styxServer.Handler = styx.Stack(logrequests, &srv)
+
+	log.Fatal(styxServer.ListenAndServe())
+}
+
+func walkTo(v interface{}, loc string) (interface{}, interface{}, bool) {
+	cwd := v
+	parts := strings.FieldsFunc(loc, func(r rune) bool { return r == '/' })
+	var parent interface{}
+
+	for _, p := range parts {
+		switch v := cwd.(type) {
+		case map[string]interface{}:
+			parent = v
+			if child, ok := v[p]; !ok {
+				return nil, nil, false
+			} else {
+				cwd = child
+			}
+		case []interface{}:
+			parent = v
+			i, err := strconv.Atoi(p)
+			if err != nil {
+				return nil, nil, false
+			}
+			if len(v) <= i {
+				return nil, nil, false
+			}
+			cwd = v[i]
+		default:
+			return nil, nil, false
+		}
+	}
+	return parent, cwd, true
+}
+
+func (srv *server) Serve9P(s *styx.Session) {
+	for s.Next() {
+		t := s.Request()
+		parent, file, ok := walkTo(srv.file, t.Path())
+		if !ok {
+			t.Rerror("no such file or directory")
+			continue
+		}
+		fi := &stat{name: path.Base(t.Path()), file: &fakefile{v: file}}
+		switch t := t.(type) {
+		case styx.Twalk:
+			t.Rwalk(fi, nil)
+		case styx.Topen:
+			switch v := file.(type) {
+			case map[string]interface{}, []interface{}:
+				t.Ropen(mkdir(v), nil)
+			default:
+				os.Stderr.WriteString("opening")
+				t.Ropen(&fakefile{v: file}, nil)
+			}
+		case styx.Tutimes:
+			t.Rutimes(nil)
+		case styx.Ttruncate:
+			t.Rtruncate(nil)
+		/*case styx.Wstat:
+		t.Rstat(fi, nil)*/
+		case styx.Tstat:
+			t.Rstat(fi, nil)
+		case styx.Tcreate:
+			switch v := file.(type) {
+			case map[string]interface{}:
+				if t.Mode.IsDir() {
+					dir := make(map[string]interface{})
+					v[t.Name] = dir
+					t.Rcreate(mkdir(dir), nil)
+				} else {
+					v[t.Name] = new(bytes.Buffer)
+					t.Rcreate(&fakefile{
+						v:   v[t.Name],
+						set: func(s string) { v[t.Name] = s },
+					}, nil)
+				}
+			case []interface{}:
+				i, err := strconv.Atoi(t.Name)
+				if err != nil {
+					t.Rerror("member of an array must be a number: %s", err)
+					break
+				}
+				if t.Mode.IsDir() {
+					dir := make(map[string]interface{})
+					v[i] = dir
+					t.Rcreate(mkdir(dir), nil)
+				} else {
+					v[i] = new(bytes.Buffer)
+					t.Rcreate(&fakefile{
+						v:   v[i],
+						set: func(s string) { v[i] = s },
+					}, nil)
+				}
+			default:
+				t.Rerror("%s is not a directory", t.Path())
+			}
+		case styx.Tremove:
+			switch v := file.(type) {
+			case map[string]interface{}:
+				if len(v) > 0 {
+					t.Rerror("directory is not empty")
+					break
+				}
+				if parent != nil {
+					if m, ok := parent.(map[string]interface{}); ok {
+						delete(m, path.Base(t.Path()))
+						t.Rremove(nil)
+					} else {
+						t.Rerror("cannot delete array element yet")
+						break
+					}
+				} else {
+					t.Rerror("permission denied")
+				}
+			default:
+				if parent != nil {
+					if m, ok := parent.(map[string]interface{}); ok {
+						delete(m, path.Base(t.Path()))
+						t.Rremove(nil)
+					} else {
+						t.Rerror("cannot delete array element")
+					}
+				} else {
+					t.Rerror("permission denied")
+				}
+			}
+		}
+	}
+}
