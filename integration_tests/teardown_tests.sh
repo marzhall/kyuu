fd2fde0c494b74515cbb4862960eb20d44ef6f71
Marshall Conover <marzhall.o+github@gmail.com>
2019-11-09 10:15:26 -0500

    Make test teardown more robust to kyuuD errors.

--- integration_tests/teardown_tests.sh
+++ integration_tests/teardown_tests.sh
@@ -1,6 +1,8 @@
 echo "Tearing down tests."
 # wait for the server to stop handling any remaining read/write calls
 sleep 1
-fusermount -u test_kyuus
 killall kyuuD
+# in the event kyuuD has failed, wait a moment for fusermount to see that
+sleep 1
+fusermount -u test_kyuus
 rm -rf test_kyuus
